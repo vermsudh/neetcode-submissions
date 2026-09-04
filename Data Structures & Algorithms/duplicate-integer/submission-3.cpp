3d4d1e3f297ebc2dbebd88c161050bd3cc234948
Sudhanshu Verma <65793304+vermsudh@users.noreply.github.com>
2026-09-04 19:48:20 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+
+        //most optimized approach using hashset
+
+        unordered_set<int> seen;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            seen.insert(nums[i]);
+        }
+        if(nums.size() == seen.size()){
+                return false;
+        }else{
+            return true;
+        }
+        return 0;
+
+        // //sorted vector
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+
+        // for(int i = 0; i < n - 1; i++){
+        //     if(nums[i] == nums[i + 1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //brute force
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+    }
+};
