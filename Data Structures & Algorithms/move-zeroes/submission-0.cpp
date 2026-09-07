b1d62d0d05eeff9515b154f6d695623b3e405101
Sudhanshu Verma <65793304+vermsudh@users.noreply.github.com>
2026-09-07 11:13:39 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        //brute force (nested loop)
+        for(int i = 0; i < n; i++){
+            for(int j = i +1; j < n; j++){
+                if(nums[i] == 0){
+                    swap(nums[i], nums[j]);
+                }
+            }
+        }
+    }
+};
